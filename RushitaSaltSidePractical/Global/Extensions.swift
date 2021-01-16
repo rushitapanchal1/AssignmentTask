1893be1f9df4fa9b07f1579b582fbc834177eca4
Rushita Panchal <rushitapanchal1@gmail.com>
2021-01-16 11:24:19 +0530

    initial commit

---
+++ RushitaSaltSidePractical/Global/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  RushitaSaltSidePractical
+//
+//  Created by Rushita Panchal on 16/01/21.
+//  Copyright © 2021 Rushita Panchal. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension UILabel {
+    
+}
+
---
+++ RushitaSaltSidePractical/Listing/View/Cell/ListViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ListViewCell.swift
+//  RushitaSaltSidePractical
+//
+//  Created by Rushita Panchal on 16/01/21.
+//  Copyright © 2021 Rushita Panchal. All rights reserved.
+//
+
+import UIKit
+
+class ListViewCell: UITableViewCell {
+
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelDesc: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+}
